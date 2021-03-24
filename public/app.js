b6298d70e2abbf38b9c0c7d830d66708a3e54d7f
Emanuel Filipe <efilipecode@gmail.com>
2021-03-23 23:18:52 -0300

    Compiled app

---
+++ public/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#toFrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.value);
+});
