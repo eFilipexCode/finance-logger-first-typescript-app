73ba5fc61750da4e953b81841e82f57a1fc6886f
Emanuel Filipe <efilipecode@gmail.com>
2021-03-23 23:19:28 -0300

    Update app.ts

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.value);
+});
