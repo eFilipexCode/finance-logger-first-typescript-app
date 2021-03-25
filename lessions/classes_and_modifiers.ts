6cd1475c397496109b1e79a4946ef505b58e3c10
Emanuel Filipe <efilipecode@gmail.com>
2021-03-24 21:05:58 -0300

    Classes and its modifiers

--- lessions/classes_and_modifiers.ts
+++ lessions/classes_and_modifiers.ts
@@ -1,17 +1,27 @@
 class Invoice {
     // Properties this class would have...
-    // Note that they would be public by default. We can modify this behavior using the
+    // Note that they would be public (anyone can use/modify) by default. We can modify this behavior using the
     // Typescript classes' modifiers, like bellow:
-    public client: string; // - By default, but we can declare anyway
+    public client: string; // - By default... but we can declare anyway
     private details: string; // - It becomes private. Therefore, we cannot access this property in instances of this class
     readonly amount: number; // - We can only read, not change
 
+    //OBS: readonly is also true inside the class itself!
+
     // Initializing them:
-    constructor(c: string, d: string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a
-    };
+    // constructor(c: string, d: string, a: number) {
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a
+    // };
+
+    // However, we can use constructor to define the properties. 
+    // The paremeters are automatically assigned to the values.
+    constructor(
+        readonly client2: string,
+        private details2: string,
+        public amount2: number
+    ){};
 
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`
